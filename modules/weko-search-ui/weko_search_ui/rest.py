0e7e5d2d628da25f28252f344460446eeda38f98
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-06 15:12:05 +0900

    update template for item detail

--- modules/weko-search-ui/weko_search_ui/rest.py
+++ modules/weko-search-ui/weko_search_ui/rest.py
@@ -211,6 +211,7 @@ class IndexSearchResource(ContentNegotiatedMethodView):
         # aggs result identify
         rd = search_result.to_dict()
         q = request.values.get('q')
+        current_app.logger.debug("AAAAA")
         if q:
             try:
                 paths = Indexes.get_self_list(q)
@@ -253,8 +254,6 @@ class IndexSearchResource(ContentNegotiatedMethodView):
                     and len(index_info.image_name) > 0:
                     nlst[0]['img'] = index_info.image_name
             agp.append(nlst)
-
-        current_app.logger.debug(rd)
 
         return self.make_response(
             pid_fetcher=self.pid_fetcher,
