60ad59d47496baa2773192eade39a6ad30cba95d
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-06 17:45:49 +0900

    Add style setting view for indextree.

--- modules/weko-index-tree/weko_index_tree/admin.py
+++ modules/weko-index-tree/weko_index_tree/admin.py
@@ -43,7 +43,7 @@ class IndexSettingView(BaseView):
                     height = request.form.get('height', '1')
 
                     data={'width':width, 'height':height}
-                    IndexStyle.create('weko')
+                    IndexStyle.create('weko', width=width, height=height)
 
 
                     flash(_('The information was created.'), category='success')
