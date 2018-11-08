efa317e970a67c1a782eb75ab2636061fd9bcade
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-08 15:24:27 +0900

    Add style setting view for indextree.

--- modules/weko-theme/weko_theme/static/js/weko_theme/base.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/base.js
@@ -3,11 +3,23 @@ require([
   "bootstrap"
   ], function() {
     $(document).ready(function() {
-        $('#btn_edit_start').on('click', function(){
-          window.location.href = '/schema/list/';
-        });
-        $('#btn_edit_stop').on('click', function(){
-          window.location.href = '/';
-        });
+      $('#btn_edit_start').on('click', function(){
+        window.location.href = '/schema/list/';
+      });
+      $('#btn_edit_stop').on('click', function(){
+        window.location.href = '/';
+      });
     });
+
+    $(window).on('load resize', function(){
+      alert('Browser Resized!!!')
+
+
+
+
+
+    });
+
+
+
 });
