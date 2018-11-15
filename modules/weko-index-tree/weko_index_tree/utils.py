eb39c3bfcf3c1f139a37822512ef9be3338ab23f
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-15 17:27:40 +0900

    Add view for setting display number of indextree.

--- modules/weko-index-tree/weko_index_tree/utils.py
+++ modules/weko-index-tree/weko_index_tree/utils.py
@@ -82,6 +82,7 @@ def get_tree_json(obj, pid=0):
     :param obj:
     :return:
     """
+    flash(obj)
 
     def get_settings():
         return dict(emitLoadNextLevel=False,
@@ -286,4 +287,4 @@ def reduce_index_by_more(tree):
     #         flash('Before Set!!')
     #         tree[0]['children'] = new
     #
-    flash(tree)
+    # flash(tree)
