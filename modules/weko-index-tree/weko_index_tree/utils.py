30b34906ec6d6a9e8a071437fce0597839e422ab
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-19 18:15:31 +0900

    Add view for setting display number of indextree.

--- modules/weko-index-tree/weko_index_tree/utils.py
+++ modules/weko-index-tree/weko_index_tree/utils.py
@@ -273,8 +273,7 @@ def reduce_index_by_more(tree, more_ids=[]):
                 more_node = {"children": [],
                              "emitLoadNextLevel": False,
                              "id": "more",
-                             # "settings": {"checked": False, "isCollapsedOnInit": True},
-                             "settings": {'cssClass': 'fa fa-arrow-down'},
+                             "settings": {"checked": False, "isCollapsedOnInit": True},
                              "value": "more..."}
                 children.insert(len(children), more_node)
 
