d2ce2718512a1a09289c87e179848ee81fd8b8b2
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-21 16:21:01 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/query.py
+++ modules/weko-search-ui/weko_search_ui/query.py
@@ -554,11 +554,11 @@ def item_path_search_factory(self, search, index_id=None):
     from invenio_records_rest.sorter import default_sorter_factory
     search_index = search._index[0]
     search, sortkwargs = default_sorter_factory(search, search_index)
+    current_app.logger.debug(sortkwargs)
     for key, value in sortkwargs.items():
         urlkwargs.add(key, value)
 
     urlkwargs.add('q', query_q)
-    current_app.logger.debug(urlkwargs)
     return search, urlkwargs
 
 
