68df4b7c2ae877ebeeb0e83f29beb860c1a88ca3
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-21 15:04:37 +0900

    add template

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -444,7 +444,7 @@ class WekoDeposit(Deposit):
         # Save Index Path on ES
         jrc.update(dict(path=index_lst))
         # add at 20181121 start
-        sub_sort={"1":0}
+        sub_sort=[{"1":0}]
         # for pth in index_lst:
         #     sub_sort[pth[-13:]]='0'
         jrc.update(dict(custom_sort=sub_sort))
