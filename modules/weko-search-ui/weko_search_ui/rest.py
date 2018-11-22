9b6158a3c9f33bada573ff5c5b617089d248fbe2
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-22 18:28:26 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/rest.py
+++ modules/weko-search-ui/weko_search_ui/rest.py
@@ -184,7 +184,8 @@ class IndexSearchResource(ContentNegotiatedMethodView):
         search = search_obj.with_preference_param().params(version=True)
         search = search[(page - 1) * size:page * size]
         search, qs_kwargs = self.search_factory(self, search)
-        urlkwargs.update(search._sort)
+        urlkwargs.update(qs_kwargs)
+        current_app.logger.debug(search._sort)
         # Execute search
         search_result = search.execute()
 
