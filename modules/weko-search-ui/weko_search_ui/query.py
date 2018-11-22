e37fd0b350e3f6a3073d390590b020c80eab7b57
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-22 16:09:41 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/query.py
+++ modules/weko-search-ui/weko_search_ui/query.py
@@ -557,7 +557,7 @@ def item_path_search_factory(self, search, index_id=None):
 
     script_str={
         "_script": {
-            "script":"doc['custom_sort'].get('1539652608824').value!=6 ? 1 : 2",
+            "script":"doc['custom_sort'].get('1539652608824').value!=5 ? 1 : 2",
             "type": "number",
             "params": {
                 "in_id": "1539652608824"
