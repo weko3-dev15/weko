f8b10640eee779024fb28c80518c483d7c16bc0b
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-26 17:53:56 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
+++ modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
@@ -91,23 +91,23 @@
 //        $("#tab_display").addClass("active")
 
 
-        var data = $rootScope.vm
+        var data = $rootScope.vm.invenioSearchResults.hits.hits
         alert(data)
 
 
-        // request api
-        $http({
-            method: 'POST',
-            url: '/item_management/save',
-            data: {
-            'recid':"123"
-          },
-          headers: {'Content-Type': 'application/json'},
-        }).then(function successCallback(response) {
-          window.location.href = '/search?search_type=2&q=1539652608824'
-        }, function errorCallback(response) {
-
-        });
+//        // request api
+//        $http({
+//            method: 'POST',
+//            url: '/item_management/save',
+//            data: {
+//            'recid':"123"
+//          },
+//          headers: {'Content-Type': 'application/json'},
+//        }).then(function successCallback(response) {
+//          window.location.href = '/search?search_type=2&q=1539652608824'
+//        }, function errorCallback(response) {
+//
+//        });
      }
 
      $scope.itemManagementCancel= function(){
