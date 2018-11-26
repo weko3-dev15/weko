076ff6f573c1edf90bbf59aceebcac9428cc6903
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-26 19:37:53 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
+++ modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
@@ -91,7 +91,7 @@
            var sub = {"id":"", custom_sort:""}
            sub.id= x.id;
 //           sub.custom_sort=;
-           alert(JSON.stringily(x))
+           alert(JSON.stringify(x))
            custom_sort_list.push(sub);
         }
         var post_data ={"q_id":$rootScope.index_id_q, "sort":custom_sort_list}
