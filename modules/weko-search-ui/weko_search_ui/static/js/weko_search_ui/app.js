308abd83cb0188d0d2e519ce3b7ddcb9de1cd110
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-26 16:11:20 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
+++ modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
@@ -1,49 +1,49 @@
-//  require([
-//    "jquery",
-//    "bootstrap",
-//    "node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker"
-//    ], function() {
-//    // loading all the jQuery modules for the not require.js ready scripts
-//    // everywhere.
-//    $(function(){
-//      $('#myModal').modal({
-//        show: false
-//      })
-//      page_global = {
-//        queryObj: null
-//      }
-//      page_global.queryObj = query_to_hash();
-//      $('#page_count').val(page_global.queryObj['size'])
-//      $('#page_count').on('change', function(){
-//        if(page_global.queryObj['size'] != $('#page_count').val()) {
-//          page_global.queryObj['size'] = $('#page_count').val();
-//          queryStr = hash_to_query(page_global.queryObj);
-//          window.location.href = window.location.pathname + '?' + queryStr;
-//        }
-//      });
-//      function query_to_hash(queryString) {
-//        var query = queryString || location.search.replace(/\?/, "");
-//        return query.split("&").reduce(function(obj, item, i) {
-//          if(item) {
-//            item = item.split('=');
-//            obj[item[0]] = item[1];
-//            return obj;
-//          }
-//        }, {});
-//      };
-//      function hash_to_query(queryObj) {
-//        var str = '';
-//        Object.keys(queryObj).forEach(function(key){
-//          if(str.length > 0) {
-//            str = str + '&' + key + '=' + queryObj[key];
-//          } else {
-//            str = key + '=' + queryObj[key];
-//          }
-//        });
-//        return str;
-//      }
-//    });
-//});
+  require([
+    "jquery",
+    "bootstrap",
+    "node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker"
+    ], function() {
+    // loading all the jQuery modules for the not require.js ready scripts
+    // everywhere.
+    $(function(){
+      $('#myModal').modal({
+        show: false
+      })
+      page_global = {
+        queryObj: null
+      }
+      page_global.queryObj = query_to_hash();
+      $('#page_count').val(page_global.queryObj['size'])
+      $('#page_count').on('change', function(){
+        if(page_global.queryObj['size'] != $('#page_count').val()) {
+          page_global.queryObj['size'] = $('#page_count').val();
+          queryStr = hash_to_query(page_global.queryObj);
+          window.location.href = window.location.pathname + '?' + queryStr;
+        }
+      });
+      function query_to_hash(queryString) {
+        var query = queryString || location.search.replace(/\?/, "");
+        return query.split("&").reduce(function(obj, item, i) {
+          if(item) {
+            item = item.split('=');
+            obj[item[0]] = item[1];
+            return obj;
+          }
+        }, {});
+      };
+      function hash_to_query(queryObj) {
+        var str = '';
+        Object.keys(queryObj).forEach(function(key){
+          if(str.length > 0) {
+            str = str + '&' + key + '=' + queryObj[key];
+          } else {
+            str = key + '=' + queryObj[key];
+          }
+        });
+        return str;
+      }
+    });
+});
 
 (function (angular) {
   // Bootstrap it!
@@ -115,6 +115,7 @@
     searchResCtrl.$inject = [
       '$scope',
       '$rootScope',
+      '$http'
     ];
     angular.module('searchResult.controllers')
       .controller('searchResCtrl', searchResCtrl);
