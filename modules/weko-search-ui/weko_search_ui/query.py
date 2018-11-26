197492da28a26c5404acca399567b078cee33a78
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-26 13:36:22 +0900

    Add contents size limitation for uploading.

--- modules/weko-search-ui/weko_search_ui/query.py
+++ modules/weko-search-ui/weko_search_ui/query.py
@@ -537,6 +537,7 @@ def item_path_search_factory(self, search, index_id=None):
 
     # create a index search query
     query_q = _get_index_earch_query()
+    flash(str(query_q))
 
     urlkwargs = MultiDict()
     try:
