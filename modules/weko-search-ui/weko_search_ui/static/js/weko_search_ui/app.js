dd0196d7161f505134da4e4fe3a224a8a3d5266b
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-26 19:28:36 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
+++ modules/weko-search-ui/weko_search_ui/static/js/weko_search_ui/app.js
@@ -86,7 +86,7 @@
 
      $scope.itemManagementSave= function(){
         var data = $scope.vm.invenioSearchResults.hits.hits
-        alert(data)
+        alert(JSON.stringify(data))
         var custom_sort_list =[]
         for(var x in data){
            var sub = {"id":"", custom_sort:""}
