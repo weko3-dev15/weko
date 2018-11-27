853bd35d7ac88861c564e7d284c6f2a610c8bb84
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-27 11:17:30 +0900

    Add contents size limitation for uploading.

--- modules/weko-search-ui/weko_search_ui/query.py
+++ modules/weko-search-ui/weko_search_ui/query.py
@@ -449,7 +449,7 @@ def item_path_search_factory(self, search, index_id=None):
 
         query_q = {
             "_source": {
-                "excludes": ['content']
+                "excludes": ['content', '*.*.file']
             },
             "query": {
                 "match": {
