208547061d772328d12bd8e1596cd55842b3b8d0
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-27 18:49:02 +0900

    add template

--- modules/weko-search-ui/weko_search_ui/query.py
+++ modules/weko-search-ui/weko_search_ui/query.py
@@ -562,7 +562,7 @@ def item_path_search_factory(self, search, index_id=None):
             factor_obj = Indexes.get_item_sort(ind_id)
             script_str = {
                 "_script": {
-                    "script": "factor.get(doc[\"control_number\"].value) !=0 ? factor.get(doc[\"control_number\"].value):Integer.MAX_VALUE",
+                    "script": "factor.get(doc[\"control_number\"].value)&&factor.get(doc[\"control_number\"].value) !=0 ? factor.get(doc[\"control_number\"].value):Integer.MAX_VALUE",
                     "type": "number",
                     "params": {
                         "factor": factor_obj
@@ -575,7 +575,7 @@ def item_path_search_factory(self, search, index_id=None):
             search._sort.append(script_str)
             search._sort.append(default_sort)
             urlkwargs.add(key, value)
-        # set selectbox 
+        # set selectbox
         urlkwargs.add(key, value)
 
     urlkwargs.add('q', query_q)
