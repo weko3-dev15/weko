30ff5a2bbce1937eef9dfadfc0f8ca5e7d005e6a
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-28 12:24:04 +0900

    Add contents size limitation for uploading.

--- modules/weko-records-ui/weko_records_ui/views.py
+++ modules/weko-records-ui/weko_records_ui/views.py
@@ -262,13 +262,15 @@ def default_view_method(pid, record, template=None, **kwargs):
     # Get index style
     style = IndexStyle.get(current_app.config['WEKO_INDEX_TREE_STYLE_OPTIONS']['id'])
     width = style.width if style else '3'
+    height = style.height if style else '300'
 
     return render_template(
         template,
         pid=pid,
         record=record,
         community_id=community_id,
+        width=width,
+        height=height,
         **ctx,
-        **kwargs,
-        width=width
+        **kwargs
     )
