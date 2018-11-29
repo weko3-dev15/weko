773505228d90ebdc2513c323cf0fb2dc2f1fc2c6
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-29 14:39:28 +0900

    Add contents size limitation for uploading.

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -377,6 +377,7 @@ class WekoDeposit(Deposit):
                             except Exception as e:
                                 abort(500, '{}'.format(e.errors))
                             break
+            flash(self.jrc)
             self.jrc.update({'content': fmd})
 
     def get_file_data(self):
