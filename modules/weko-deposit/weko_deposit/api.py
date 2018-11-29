f1b63f47b60dc39947ba83ccd69d166625ba5475
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-11-29 12:50:38 +0900

    Add contents size limitation for uploading.

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -338,11 +338,13 @@ class WekoDeposit(Deposit):
                 if self.jrc.get('_item_metadata'):
                     meta = self.jrc['_item_metadata']
                     if meta.get('item_1542872214361'):
-                        values = meta['item_1542872214361']['attribute_value_mlt']
-                        for metadata in values:
-                            if metadata.get('file'):
-                                flash('Delete file!!!!!')
-                                del metadata['file']
+                        flash('Delete item_1542872214361!!!!!')
+                        del meta['item_1542872214361']
+                        # values = meta['item_1542872214361']['attribute_value_mlt']
+                        # for metadata in values:
+                        #     if metadata.get('file'):
+                        #         flash('Delete file!!!!!')
+                        #         del metadata['file']
 
 
                 self.indexer.upload_metadata(self.jrc, self.pid.object_uuid,
