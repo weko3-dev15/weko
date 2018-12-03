0f90c6169dae422d71780d874effb6105132fc9a
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-03 17:32:36 +0900

    Add opensearch rss format.

--- modules/weko-records/weko_records/serializers/rss.py
+++ modules/weko-records/weko_records/serializers/rss.py
@@ -234,12 +234,11 @@ class RssSerializer(JSONSerializer):
                     creator_metadata = self.get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
-                    return str(item_map)
+                    return str(creator_metadata)
 
                     creator_names = creator_metadata[
                         item_map[_creatorName_value]]
-                    creator_name_langs = creator_metadata[
-                        item_map[_creatorName_attr_lang]]
+                    creator_name_langs = creator_metadata[item_id + '.' + '']
 
                     if creator_name_langs:
                         if isinstance(creator_name_langs, list):
