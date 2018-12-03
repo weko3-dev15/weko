114ce59bbebac5a4c610ac83224896b8f70d1b45
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-03 11:12:23 +0900

    Add opensearch rss format.

--- modules/weko-records/weko_records/serializers/rss.py
+++ modules/weko-records/weko_records/serializers/rss.py
@@ -151,8 +151,7 @@ class RssSerializer(JSONSerializer):
             # Set link
             _pid = item_metadata['control_number']
             item_url = request.host_url + 'records/' + _pid
-            fe.link(link=item_url)
-            # fe.link(href=item_url, rel='alternate', type='text/xml')
+            fe.link(href=item_url, rel='alternate', type='text/xml')
 
             return fg.rss_str(pretty=True)
 
