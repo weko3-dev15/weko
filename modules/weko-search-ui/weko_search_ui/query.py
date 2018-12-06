1141001109c72ff5e7d1a01617d0c8bd45d44a8c
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-12-06 17:17:58 +0900

    data save

--- modules/weko-search-ui/weko_search_ui/query.py
+++ modules/weko-search-ui/weko_search_ui/query.py
@@ -605,14 +605,14 @@ def item_path_search_factory(self, search, index_id=None):
         # set selectbox
         urlkwargs.add(key, value)
         # defalult sort
-    # default sort
-    if not sortkwargs:
-        import weko_admin
-        sort_key, sort = SearchSetting.get_default_sort(current_app.config['WEKO_SEARCH_TYPE_INDEX'])
-        if sort == 'desc':
-            sort_key = '-'+ sort_key
-
-        urlkwargs.add('sort', sort_key)
+    # # default sort
+    # if not sortkwargs:
+    #     import weko_admin
+    #     sort_key, sort = SearchSetting.get_default_sort(current_app.config['WEKO_SEARCH_TYPE_INDEX'])
+    #     if sort == 'desc':
+    #         sort_key = '-'+ sort_key
+    #
+    #     urlkwargs.add('sort', sort_key)
 
     urlkwargs.add('q', query_q)
 
