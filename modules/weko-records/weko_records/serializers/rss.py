c1ac0ea9cab2036f7053731ce4ee496d46c5d87c
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-07 10:20:47 +0900

    Add height setting function for indextree panel.

--- modules/weko-records/weko_records/serializers/rss.py
+++ modules/weko-records/weko_records/serializers/rss.py
@@ -30,6 +30,7 @@ import pytz, copy
 from .opensearch import OpensearchExtension, OpensearchEntryExtension
 from .prism import PrismExtension, PrismEntryExtension
 from .dc import DcWekoBaseExtension, DcWekoEntryExtension
+from .utils import get_mapping, get_metadata_from_map
 from weko_index_tree.api import Index
 
 class RssSerializer(JSONSerializer):
@@ -119,7 +120,7 @@ class RssSerializer(JSONSerializer):
             if item_type_id in jpcoar_map:
                 item_map = jpcoar_map[item_type_id]
             else:
-                item_map = self.get_mapping(type_mapping, 'jpcoar_mapping')
+                item_map = get_mapping(type_mapping, 'jpcoar_mapping')
                 jpcoar_map[item_type_id] = item_map
 
             fe = fg.add_entry()
@@ -175,7 +176,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    type_metadata = self.get_metadata_from_map(
+                    type_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     aggregationTypes = type_metadata[item_map[_aggregationType]]
@@ -196,7 +197,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    file_metadata = self.get_metadata_from_map(
+                    file_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     mime_types = file_metadata[item_map[_mimeType]]
@@ -214,7 +215,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    uri_metadata = self.get_metadata_from_map(
+                    uri_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     uri_list = uri_metadata[item_map[_uri]]
@@ -233,7 +234,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    creator_metadata = self.get_metadata_from_map(
+                    creator_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     creator_names = creator_metadata[item_map[_creatorName_value]]
@@ -270,7 +271,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    publisher_metadata = self.get_metadata_from_map(
+                    publisher_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     publisher_names = publisher_metadata[
@@ -321,7 +322,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    sourceTitle_metadata = self.get_metadata_from_map(
+                    sourceTitle_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     source_titles = sourceTitle_metadata[
@@ -342,7 +343,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    sourceIdentifier_metadata = self.get_metadata_from_map(
+                    sourceIdentifier_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     source_identifiers = sourceIdentifier_metadata[
@@ -368,7 +369,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    volume_metadata = self.get_metadata_from_map(
+                    volume_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     volumes = volume_metadata[item_map[_volume]]
@@ -387,7 +388,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    issue_metadata = self.get_metadata_from_map(
+                    issue_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     issues = issue_metadata[item_map[_issue]]
@@ -406,7 +407,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    pageStart_metadata = self.get_metadata_from_map(
+                    pageStart_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     pageStarts = pageStart_metadata[item_map[_pageStart]]
@@ -425,7 +426,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    pageEnd_metadata = self.get_metadata_from_map(
+                    pageEnd_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     pageEnds = pageEnd_metadata[item_map[_pageEnd]]
@@ -445,7 +446,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    date_metadata = self.get_metadata_from_map(
+                    date_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     dates = date_metadata[item_map[_date]]
@@ -469,7 +470,7 @@ class RssSerializer(JSONSerializer):
 
                 # Get item data
                 if item_id in item_metadata:
-                    description_metadata = self.get_metadata_from_map(
+                    description_metadata = get_metadata_from_map(
                         item_metadata[item_id], item_id)
 
                     descriptions = description_metadata[
@@ -515,84 +516,84 @@ class RssSerializer(JSONSerializer):
 
         return fg.rss_str(pretty=True)
 
-    def get_mapping(self, item_type_mapping, mapping_type):
-        """
-        Format itemtype mapping data. [Key:Schema, Value:ItemId]
-        :param item_type_mapping:
-        :param mapping_type:
-        :return:
-        """
+    # def get_mapping(self, item_type_mapping, mapping_type):
+    #     """
+    #     Format itemtype mapping data. [Key:Schema, Value:ItemId]
+    #     :param item_type_mapping:
+    #     :param mapping_type:
+    #     :return:
+    #     """
+    #
+    #     def get_schema_key_info(schema, parent_key, schema_json={}):
+    #
+    #         for k, v in schema.items():
+    #             key = parent_key + '.' + k if parent_key else k
+    #             if isinstance(v, dict):
+    #                 child_key = copy.deepcopy(key)
+    #                 get_schema_key_info(v, child_key, schema_json)
+    #             else:
+    #                 schema_json[key] = v
+    #
+    #         return schema_json
+    #
+    #     schema = {}
+    #     for item_id, maps in item_type_mapping.items():
+    #         if isinstance(maps[mapping_type], dict):
+    #             item_schema = get_schema_key_info(maps[mapping_type], '', {})
+    #             for k, v in item_schema.items():
+    #                 item_schema[k] = item_id + '.' + v if v else item_id
+    #             schema.update(item_schema)
+    #
+    #     return schema
 
-        def get_schema_key_info(schema, parent_key, schema_json={}):
-
-            for k, v in schema.items():
-                key = parent_key + '.' + k if parent_key else k
-                if isinstance(v, dict):
-                    child_key = copy.deepcopy(key)
-                    get_schema_key_info(v, child_key, schema_json)
-                else:
-                    schema_json[key] = v
-
-            return schema_json
-
-        schema = {}
-        for item_id, maps in item_type_mapping.items():
-            if isinstance(maps[mapping_type], dict):
-                item_schema = get_schema_key_info(maps[mapping_type], '', {})
-                for k, v in item_schema.items():
-                    item_schema[k] = item_id + '.' + v if v else item_id
-                schema.update(item_schema)
-
-        return schema
-
-    def get_metadata_from_map(self, item_data, item_id):
-        """
-        Get item metadata from search result.
-        :param item_data:
-        :param item_id:
-        :return:
-        """
-
-        def get_sub_item_data(props, parent_key=''):
-            key = parent_key if parent_key else ''
-            value = {}
-
-            if isinstance(props, list):
-                for prop in props:
-                    for k, v in prop.items():
-                        if isinstance(v, list) or isinstance(v, dict):
-                            value.update(get_sub_item_data(v, key))
-                        else:
-                            sub_key = key + '.' + k if key else k
-                            if sub_key in value:
-                                if isinstance(value[sub_key], list):
-                                    value[sub_key].append(v)
-                                else:
-                                    _value = value[sub_key]
-                                    value[sub_key] = [_value, v]
-                            else:
-                                value[sub_key] = v
-            else:
-                for k, v in props.items():
-                    if isinstance(v, list) or isinstance(v, dict):
-                        value.update(get_sub_item_data(v, key))
-                    else:
-                        sub_key = key + '.' + k if key else k
-                        if sub_key in value:
-                            if isinstance(value[sub_key], list):
-                                value[sub_key].append(v)
-                            else:
-                                _value = value[sub_key]
-                                value[sub_key] = [_value, v]
-                        else:
-                            value[sub_key] = v
-            return value
-
-        item_value = {}
-        if 'attribute_value' in item_data:
-            item_value[item_id] = item_data['attribute_value']
-        elif 'attribute_value_mlt' in item_data:
-            item_value.update(get_sub_item_data(item_data['attribute_value_mlt'],
-                                                item_id))
-
-        return item_value
+    # def get_metadata_from_map(self, item_data, item_id):
+    #     """
+    #     Get item metadata from search result.
+    #     :param item_data:
+    #     :param item_id:
+    #     :return:
+    #     """
+    #
+    #     def get_sub_item_data(props, parent_key=''):
+    #         key = parent_key if parent_key else ''
+    #         value = {}
+    #
+    #         if isinstance(props, list):
+    #             for prop in props:
+    #                 for k, v in prop.items():
+    #                     if isinstance(v, list) or isinstance(v, dict):
+    #                         value.update(get_sub_item_data(v, key))
+    #                     else:
+    #                         sub_key = key + '.' + k if key else k
+    #                         if sub_key in value:
+    #                             if isinstance(value[sub_key], list):
+    #                                 value[sub_key].append(v)
+    #                             else:
+    #                                 _value = value[sub_key]
+    #                                 value[sub_key] = [_value, v]
+    #                         else:
+    #                             value[sub_key] = v
+    #         else:
+    #             for k, v in props.items():
+    #                 if isinstance(v, list) or isinstance(v, dict):
+    #                     value.update(get_sub_item_data(v, key))
+    #                 else:
+    #                     sub_key = key + '.' + k if key else k
+    #                     if sub_key in value:
+    #                         if isinstance(value[sub_key], list):
+    #                             value[sub_key].append(v)
+    #                         else:
+    #                             _value = value[sub_key]
+    #                             value[sub_key] = [_value, v]
+    #                     else:
+    #                         value[sub_key] = v
+    #         return value
+    #
+    #     item_value = {}
+    #     if 'attribute_value' in item_data:
+    #         item_value[item_id] = item_data['attribute_value']
+    #     elif 'attribute_value_mlt' in item_data:
+    #         item_value.update(get_sub_item_data(item_data['attribute_value_mlt'],
+    #                                             item_id))
+    #
+    #     return item_value
