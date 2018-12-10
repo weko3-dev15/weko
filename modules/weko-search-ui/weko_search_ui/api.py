e373898c860f63f1c862c91e958ddc54a56c2d85
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-12-10 16:19:29 +0900

    update detail page

--- modules/weko-search-ui/weko_search_ui/api.py
+++ modules/weko-search-ui/weko_search_ui/api.py
@@ -117,7 +117,7 @@ class SearchSetting(object):
     @classmethod
     def get_search_detail_keyword(cls, str):
         """Get search detail keyword"""
-        if str=='':
+        if str =='1':
             res = sm.get()
             options=None
             key_options = dict()
@@ -128,5 +128,18 @@ class SearchSetting(object):
 
             key_options['condition_setting']= options
             current_app.logger.debug('CCCCCCCC')
-            
+
+            return key_options
+        else:
+            res = sm.get()
+            options = None
+            key_options = dict()
+            if res:
+                options = res.search_conditions
+            else:
+                options = ad_config['WEKO_SEARCH_MANAGEMENT_OPTIONS'].get('detail_condition')
+
+            key_options['condition_setting'] = options
+            current_app.logger.debug('CCCCCCCC')
+
             return key_options
