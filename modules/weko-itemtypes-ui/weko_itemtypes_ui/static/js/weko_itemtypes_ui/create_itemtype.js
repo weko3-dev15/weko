5467ef529a66bbf069f2a53671b5a931641a0359
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-10 18:34:42 +0900

    Add itemtype property sort function.

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/create_itemtype.js
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/create_itemtype.js
@@ -246,10 +246,13 @@
         async: false,
         success: function(data, status){
 
-          alert(data);
-//          page_global.table_row_map.schema.properties["title_ja"] = data['title']['schema'}['title_ja']
-//          page_global.table_row_map.schema.properties["title_en"] = data['title']['schema'}['title_en']
-//          page_global.table_row_map.form.push(data[key].form);
+//          alert(data['title']['schema'});
+//          for (var key in data) {
+//
+//          }
+          page_global.table_row_map.schema.properties["title_ja"] = data['title']['schema']['title_ja']
+          page_global.table_row_map.schema.properties["title_en"] = data['title']['schema']['title_en']
+          page_global.table_row_map.form.push(data[key].form);
 
         },
         error: function(status, error){
