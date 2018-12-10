3ca8cf4eea6d2a72a730d0c3975d587bb35b9ac2
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-10 13:22:17 +0900

    For opensearch debug.

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/create_itemtype.js
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/create_itemtype.js
@@ -1032,10 +1032,12 @@
       async: false,
       success: function(data, status){
         properties_obj = data;
-        alert(str(data.keys()));
-        Object.keys(data).forEach(function(key) {
-          select_option = select_option + '<option value="cus_'+key+'">'+data[key].name+'</option>'
-        });
+        for (var key in data) {
+          select_option = select_option + '<option value="cus_'+key+'">'+data[key].name+'</option>';
+        }
+//        Object.keys(data).forEach(function(key) {
+//          select_option = select_option + '<option value="cus_'+key+'">'+data[key].name+'</option>'
+//        });
       },
       error: function(status, error){
         console.log(error);
