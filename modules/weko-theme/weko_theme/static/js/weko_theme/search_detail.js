08366def46cba9fa3e0d27cf4a704b88829ea1f3
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-12-12 10:37:26 +0900

    update detail page

--- modules/weko-theme/weko_theme/static/js/weko_theme/search_detail.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/search_detail.js
@@ -58,7 +58,7 @@
             $scope.change_search_key = function (index, search_key) {
                 var obj = $scope.get_search_key(search_key)
                 $scope.condition_data.splice(index, 1, obj);
-                $scope.update_disabled_flg();
+//                $scope.update_disabled_flg();
             }
             // detail search
             $scope.detail_search = function () {
