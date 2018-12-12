603873ed930609c001c6f45d161c8601d2bc8f30
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-12-12 18:20:30 +0900

    update detail page

--- modules/weko-search-ui/weko_search_ui/views.py
+++ modules/weko-search-ui/weko_search_ui/views.py
@@ -73,7 +73,7 @@ def search():
     else:
         return render_template(current_app.config['SEARCH_UI_SEARCH_TEMPLATE'],
                                index_id=cur_index_id, community_id=community_id,
-                               sort_option=sort_options, disply_setting=disply_setting, width=width, **ctx)
+                               sort_option=sort_options, **disply_setting, width=width, **ctx)
 
 
 @blueprint_api.route('/opensearch/description.xml', methods=['GET'])
