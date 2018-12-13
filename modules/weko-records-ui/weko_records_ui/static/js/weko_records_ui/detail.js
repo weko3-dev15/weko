c11ee7cd48f431f887711f92b27830b137ea7ea7
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-13 19:13:25 +0900

    Modify preview screen.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
@@ -10,7 +10,7 @@ require([
   $('.panel-toggle').on('click', function(){
     href = $(this).attr("href");
     contentClass = $(href).attr("class");
-    alert($(href).attr("class"));
+    alert($(href).attr("id"));
 //    if(contentClass === 'collapse') {
 //      $(href).removeClass();
 //      $(href).addClass('collapse in');
