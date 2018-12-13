6e4d09b8835f3b4c927ca4747fd39dddaf9953fe
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-12-13 09:38:34 +0900

    Modify workflow screen for responsive design.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
@@ -5,29 +5,6 @@ require([
   $('#btn_back').on('click', function(){
     window.history.back();
   });
-//  $('.preview-link').on('click', function(event) {
-//    $('#preview').show();
-//    var filename = encodeURIComponent($(event.target).data('filename'));
-//    $('#preview-iframe').attr("src","{{ url_for('invenio_records_ui.recid_preview', pid_value=record.recid, filename='') }}" + filename);
-//  });
-//  TODO
-  $('.panel-toggle').on('click', function(){
-    href = $(this).attr("href");
-    contentClass = $(href).attr("class");
-
-    var timer = 0;
-    if (timer > 0) {clearTimeout(timer);}
-    timer = setTimeout(function () {
-      if(contentClass === 'collapse') {
-        $(href).removeClass();
-        $(href).addClass('collapse in');
-
-      } else {
-        $(href).removeClass();
-        $(href).addClass('collapse');
-      }
-    }, 300);
-  });
 
   $('#public_status_btn').on('click', function(){
      var status = $(this).val();
