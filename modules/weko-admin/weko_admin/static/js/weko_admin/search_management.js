1d7a3d2b58d557a37612a639b57a26d3696d9a48
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-12-17 14:31:04 +0900

    fixup bug of custom_sort

--- modules/weko-admin/weko_admin/static/js/weko_admin/search_management.js
+++ modules/weko-admin/weko_admin/static/js/weko_admin/search_management.js
@@ -56,10 +56,10 @@
       $scope.setSearchKeyOptions = function(){
         // init
         var deny_flg = 0;
-        angular.forEach($scope.dataJson.dlt_index_sort_options,function(item_sort_index,index,array){
+        angular.forEach($scope.dataJson.dlt_index_sort_options,function(item_sort_index,sort_index,sort_array){
           deny_flg = 0;
-          angular.forEach($scope.dataJson.sort_options.deny,function(item_deny,index,array){
-            if(item_sort_index.id==item_deny.id){
+          angular.forEach($scope.dataJson.sort_options.deny,function(item_deny,deny_index,deny_array){
+            if(item_sort_index.id ==item_deny.id){
               deny_flg = 1;
               break;
             }
