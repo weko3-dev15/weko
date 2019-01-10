902f2ead53e2ce39643f549e414afea1b287a6ad
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-10 17:05:40 +0900

    Debug for trunk design.

--- modules/weko-theme/weko_theme/static/js/weko_theme/settings.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/settings.js
@@ -7,7 +7,7 @@
  */
 
 require.config({
-  baseUrl: "js",
+  baseUrl: "/static/",
   paths: {
 //    jquery: "node_modules/jquery/jquery",
 //    bootstrap: "node_modules/bootstrap-sass/assets/javascripts/bootstrap",
