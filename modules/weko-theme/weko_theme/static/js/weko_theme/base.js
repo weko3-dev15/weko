ee068efad3ea2a7560111e5a048c6665de66856d
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-01-11 15:27:54 +0900

    Debug for trunk design.

--- modules/weko-theme/weko_theme/static/js/weko_theme/base.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/base.js
@@ -9,5 +9,31 @@ require([
       $('#btn_edit_stop').on('click', function(){
         window.location.href = '/';
       });
+
+      var options = {
+          cellHeight: 80,
+          verticalMargin: 10
+      };
+      $('.grid-stack').gridstack(options);
+
+      this.grid = $('.grid-stack').data('gridstack');
+
+      this.saveGrid = function () {
+          this.serializedData = _.map($('.grid-stack > .grid-stack-item:visible'), function (el) {
+              el = $(el);
+              var node = el.data('_gridstack_node');
+              return {
+                  x: node.x,
+                  y: node.y,
+                  width: node.width,
+                  height: node.height
+              };
+          });
+          $('#saved-data').val(JSON.stringify(this.serializedData, null, '    '));
+          return false;
+      }.bind(this);
+
+      $('#save-grid').click(this.saveGrid);
+
     });
 });
